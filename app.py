4c4ec39974e48f1fe933f74874afd637008dea1f
Burhan Uddin Baig <82936082+burhanuddinbaig@users.noreply.github.com>
2024-07-19 19:39:04 +0500

    STY-002 get fonts dir from envirnoment variables

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, jsonify, request
+from dotenv import load_dotenv
 from font import process_fonts
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+
+fonts_dir = os.getenv('FONTS_DIR')
 
 # Route for the root URL
 @app.route('/')
@@ -15,11 +21,10 @@ def post_data():
     if 'text' in data:
         text = data['text']
         fonts_directory = "E:\\fonts_directory"
-        font_data = process_fonts(fonts_directory, text)
+        font_data = process_fonts(fonts_dir, text)
         response = {'text': text, 'data': font_data}
     else:
         response = {'error': 'No text provided'}
-    print(response)
     return jsonify(response), 201
 
 if __name__ == '__main__':
